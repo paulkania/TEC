dc2634419bed72352b025f03282b3c7be5f4c696
PaulKania <p.rozehnal@gmail.com>
2019-12-04 12:37:34 -0500

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -38,23 +38,52 @@ for i in i_l:
             combined_l.append(i)
             combined_l.append(temp_l)
             temp_l=[]
-# print(combined_l)
-# for i in combined_l:
-#     print(i)
-print(len(inp_data),len(soundex_l),len(i_l))
-# print('i_l',i_l)
-# print('soundex_l',soundex_l)
-# print('inp_data',inp_data,'\n')
 
-
-# print(soundex_l,inp_data)
+for i,j in zip(range(0,len(soundex_l)-1), range(1,len(soundex_l))):
+    # print(i,j,soundex_l[i],soundex_l[j],len(soundex_l[i]),len(soundex_l[j]))
+    # if len(soundex_l[i]) != len(soundex_l[j]):#probably not strictly necessary
+    for el in range( min(len(soundex_l[i]), len(soundex_l[j]))):
+        print(inp_data[i],'&',inp_data[j],soundex_l[i][el],soundex_l[j][el],'thund3r')
+        #print('ld',jlfsh.damerau_levenshtein_distance(soundex_l[i][el], soundex_l[j][el]))
+        #print('jaro',jlfsh.jaro_distance(soundex_l[i][el], soundex_l[j][el]))
+        print('hamming',jlfsh.hamming_distance(soundex_l[i][el], soundex_l[j][el]))
+        print()
+#what I'm seeing is that the soundex tokens aren't good indicators of similarity.
+#therefore i'll move on, look at the notebook and see what's good.
+#maybe i'll try to amalgamate these to programs.
+#where it was useful was below..in accounting for special characters.
 
 for i,j in zip(range(0,len(soundex_l)-1), range(1,len(soundex_l))):
     # print(soundex_l[i],soundex_l[j])
     if soundex_l[i]==soundex_l[j]:
         print(soundex_l[i],'&&&',soundex_l[j])
-#2312312312312312312312312312312312        soundex_l.remove(soundex_l[i]) ###investigate soundex_l
-        # soundex_l.remove(inp_data[i])
+        soundex_l.remove(soundex_l[i]) ###investigate soundex_l
+        print('eee',inp_data[i])
+        inp_data.remove(inp_data[i])
+        continue
+
+    # print('altt',i,j,soundex_l[i],soundex_l[j],len(soundex_l[i]),len(soundex_l[j]))
+    # # soundex_l[i] = soundex_l[j]
+    # print('thnd',i,j,soundex_l[i],soundex_l[j],len(soundex_l[i]),len(soundex_l[j]),'\n')
+# if soundex_l[i]==soundex_l[j]:
+        # print('entry',soundex_l[i],soundex_l[j])
+    # print(min(    (range(len(soundex_l[i]))) , (range(len(soundex_l[j])))))
+    # for el in min(range(len(soundex_l[i]), range(len(soundex_l[j])))):
+    #     print(soundex_l[i],soundex_l[j],'thund3r',jlfsh.damerau_levenshtein_distance(soundex_l[i][el], soundex_l[j][el]))
+    #     # print())
+    #     print(soundex_l[i][el], soundex_l[j][el])
+
+    # c=jlfsh.damerau_levenshtein_distance(soundex_l[i], soundex_l[j])
+    # if c<5 and len(data[i])>4 and len(data[j])>4:
+#         fillup.extend([a,data[i],'&',data[j]])
+# fup=[]
+# for i in range(0, len(fillup), 4):
+#      fup.append(fillup[i : i+4])
+# fup=sorted(fup)
+# for i in range(len(fup)):
+#     print(fup[i]
+
+
 
 # for i in range(len(inp_data)):
 #     print(soundex_l[i],inp_data[i])
