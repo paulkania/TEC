61ebc22832e69996fe91c9ee0f2cd0c32573fdb0
PaulKania <p.rozehnal@gmail.com>
2019-12-05 16:22:13 -0500

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -20,18 +20,18 @@ print('\n\n')
 inp_data= pd.read_csv('seo.csv',usecols=[0],verbose=False) #potentially useful: skipinitialspace, squeeze,verbose
 inp_data=inp_data["Features"].tolist()
 
-print('000',inp_data)
 i_l=[]
 for i in inp_data:
     i=i.split()
     i_l.append(i)
 
-print('111',i_l)
-flat_l=[item for sublist in i_l for item in sublist]
-print(222,flat_l)
+# print('000',inp_data)
+# print('111',i_l)
+# flat_l=[item  for sublist in i_l for item in sublist] #
+# print(222,flat_l)
 
-temp_l=[]
-combined_l=[]
+temp_l=[] #temp holds soundex values to match length of elements, so each element is soundexed.
+combined_l=[] #sublist of combined string and soundex.
 soundex_l=[]
 # print(i_l,i_l[0])
 for i in i_l:
@@ -44,64 +44,56 @@ for i in i_l:
             combined_l.append(temp_l)
             temp_l=[]
 
+#this block makes no alteration in data.
+#it only compares sound-token elements together, and compares there similarity.
+#see conclusion below
 # for i,j in zip(range(0,len(soundex_l)-1), range(1,len(soundex_l))):
-#     # print(i,j,soundex_l[i],soundex_l[j],len(soundex_l[i]),len(soundex_l[j]))
-#     # if len(soundex_l[i]) != len(soundex_l[j]):#probably not strictly necessary
-#     for el in range( min(len(soundex_l[i]), len(soundex_l[j]))):
-        # print(inp_data[i],'&',inp_data[j],soundex_l[i][el],soundex_l[j][el],'thund3r')
-        #print('ld',jlfsh.damerau_levenshtein_distance(soundex_l[i][el], soundex_l[j][el]))
-        #print('jaro',jlfsh.jaro_distance(soundex_l[i][el], soundex_l[j][el]))
-        # print('hamming',jlfsh.hamming_distance(soundex_l[i][el], soundex_l[j][el]))
-        # print()
-#what I'm seeing is that the soundex tokens aren't good indicators of similarity.
-#therefore i'll move on, look at the notebook and see what's good.
-#maybe i'll try to amalgamate these to programs.
-#where it was useful was below..in accounting for special characters.
+#     print(i,j,soundex_l[i],soundex_l[j],len(soundex_l[i]),len(soundex_l[j]))
 
-# print(123,soundex_l)
+    # if len(soundex_l[i]) != len(soundex_l[j]):#probably not strictly necessary
 
+    # for el in range( min(len(soundex_l[i]), len(soundex_l[j]))):
+        #print(inp_data[i],'&',inp_data[j],soundex_l[i][el],soundex_l[j][el],'thund3r')
+        ## print('ld',jlfsh.damerau_levenshtein_distance(soundex_l[i][el], soundex_l[j][el]))
+        ## print('jaro',jlfsh.jaro_distance(soundex_l[i][el], soundex_l[j][el]))
+        #print('hamming',jlfsh.hamming_distance(soundex_l[i][el], soundex_l[j][el]))
+        ## print()
+# what I'm seeing is that the soundex tokens aren't good indicators of similarity. - Dynamic & Engagement are paired 2/4 [D552 E522]
+# therefore i'll move on, look at the notebook and see what's good.
+# maybe i'll try to amalgamate these to programs.
+# where it was useful was below..in accounting for special characters.
+
+#a counter for r_iter, so that the iterations of the duplicate remover function dont get out of range.
+r_iter=0
 for i,j in zip(range(0,len(soundex_l)-1), range(1,len(soundex_l))):
-    print(soundex_l[i],soundex_l[j])
-    # if soundex_l[i]==soundex_l[j]:
-    #     # print(soundex_l[i],'&&&',soundex_l[j])
-    #     soundex_l.remove(soundex_l[i]) ###investigate soundex_l
-    #     # print('eee',inp_data[i])
-    #     inp_data.remove(inp_data[i])
-    #     continue
-# print(321,soundex_l)
+    if soundex_l[i]==soundex_l[j]:
+        r_iter+=1
 
-    # print('altt',i,j,soundex_l[i],soundex_l[j],len(soundex_l[i]),len(soundex_l[j]))
-    # # soundex_l[i] = soundex_l[j]
-    # print('thnd',i,j,soundex_l[i],soundex_l[j],len(soundex_l[i]),len(soundex_l[j]),'\n')
-# if soundex_l[i]==soundex_l[j]:
-        # print('entry',soundex_l[i],soundex_l[j])
-    # print(min(    (range(len(soundex_l[i]))) , (range(len(soundex_l[j])))))
-    # for el in min(range(len(soundex_l[i]), range(len(soundex_l[j])))):
-    #     print(soundex_l[i],soundex_l[j],'thund3r',jlfsh.damerau_levenshtein_distance(soundex_l[i][el], soundex_l[j][el]))
-    #     # print())
-    #     print(soundex_l[i][el], soundex_l[j][el])
+###remove duplicates (geotargetting, google ad thing)
+for i,j in zip(range(0,len(soundex_l)-r_iter-1), range(1,len(soundex_l)-r_iter)):
+    if soundex_l[i]==soundex_l[j]:
+        r_iter+=1
+        print(soundex_l[i],'&&&',soundex_l[j],i_l[i],'+++',i_l[j])
+        i_l.remove(i_l[i])
+        soundex_l.remove(soundex_l[i])
+        print('999',i_l)
+#--------------
+print('ttt',i_l)
+#this block is great. it takes a nested list of
+    #strings which are within the list separated by commas.
+        #it then joins the separate strings together, spaced by a space in this case
+        #and appended to a new list.
+i_l_out=[]
+for i in i_l:
+    j= " ".join(i)
+    print(j)
+    i_l_out.append(j)
+# print('21',i_l_out)
+# print(len(i_l_out),len(inp_data),len(i_l),len(soundex_l),type(inp_data),type(i_l))
 
-    # c=jlfsh.damerau_levenshtein_distance(soundex_l[i], soundex_l[j])
-    # if c<5 and len(data[i])>4 and len(data[j])>4:
-#         fillup.extend([a,data[i],'&',data[j]])
-# fup=[]
-# for i in range(0, len(fillup), 4):
-#      fup.append(fillup[i : i+4])
-# fup=sorted(fup)
-# for i in range(len(fup)):
-#     print(fup[i]
+dicto=dict(zip(i_l_out,soundex_l))
+print(dicto)
 
-
-
-# for i in range(len(inp_data)):
-#     print(soundex_l[i],inp_data[i])
-
-    # if a<5 and len(data[i])>4 and len(data[j])>4:
-    #     fillup.extend([a,data[i],'&',data[j]])
-
-dicto=dict(zip(inp_data,soundex_l))
-# print(dicto)
-
-# df=pd.DataFrame.from_dict(dicto,orient='index')
-# print(df)
-# df.to_csv(r"C:\Users\prozehnal\Desktop\csv_house\csv\outputSoundex.csv")
+df=pd.DataFrame.from_dict(dicto,orient='index')
+print(df)
+df.to_csv(r"C:\Users\prozehnal\Desktop\csv_house\csv\outputSoundex.csv")
