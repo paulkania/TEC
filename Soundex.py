2827f85d2b1d7b7a39cbf7737fc2cb27876a1a7c
PaulKania <p.rozehnal@gmail.com>
2019-12-12 13:37:34 -0500

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -2,13 +2,13 @@
 #1  save as excel worksheet - otherwise (CSV, CSV-DOS, etc.) a warning will throw, so might as well not reformat.
 #2  If output excel sheet is open, and you try to run script again, error 'No13 - Permission Denied' will be thrown
 
-
 #import libraries
 import pandas as pd
 import os
 from collections import Counter
 import stringdist as sd
 import jellyfish as jlfsh
+import itertools
 
 #show current directory and files in current directory
 os.chdir(r"C:\Users\prozehnal\Desktop\csv_house\csv") #changes to correct directory from absolute reference
@@ -17,14 +17,14 @@ print(os.listdir(os.getcwd()),'<----files in current dir') #lists files in curre
 print('\n\n')
 
 #input -> output data manipulation
-inp_data= pd.read_csv('seo.csv',usecols=[0],verbose=False) #potentially useful: skipinitialspace, squeeze,verbose
+inp_data= pd.read_csv('db1.csv',usecols=[0],verbose=False) #potentially useful: skipinitialspace, squeeze,verbose
 inp_data=inp_data["Features"].tolist()
 
 i_l=[]
 for i in inp_data:
     i=i.split()
     i_l.append(i)
-
+# print(i_l)
 # print('000',inp_data)
 # print('111',i_l)
 # flat_l=[item  for sublist in i_l for item in sublist] #
@@ -47,53 +47,108 @@ for i in i_l:
 #this block makes no alteration in data.
 #it only compares sound-token elements together, and compares there similarity.
 #see conclusion below
-# for i,j in zip(range(0,len(soundex_l)-1), range(1,len(soundex_l))):
-#     print(i,j,soundex_l[i],soundex_l[j],len(soundex_l[i]),len(soundex_l[j]))
+close_ld=[]
+for i,j in zip(range(0,len(soundex_l)-1), range(1,len(soundex_l))):
+    # print('RESSSET',i,j,soundex_l[i],soundex_l[j],len(soundex_l[i]),len(soundex_l[j]))
 
     # if len(soundex_l[i]) != len(soundex_l[j]):#probably not strictly necessary
+    ld=0
+    for el in range( min(len(soundex_l[i]), len(soundex_l[j]))):
+        # print(inp_data[i],'&',inp_data[j],soundex_l[i][el],soundex_l[j][el],'thund3r')
+        ld_el= jlfsh.damerau_levenshtein_distance(soundex_l[i][el], soundex_l[j][el])
+        # print('ld',jlfsh.damerau_levenshtein_distance(soundex_l[i][el], soundex_l[j][el]))
+        # print('jaro',jlfsh.jaro_distance(soundex_l[i][el], soundex_l[j][el]))
+        # print('hamming',jlfsh.hamming_distance(soundex_l[i][el], soundex_l[j][el]))
+        ld+= ld_el
+        if ld<4:
+            close_ld.extend(inp_data[i])
+            close_ld.extend(inp_data[j])
+        # print()
 
-    # for el in range( min(len(soundex_l[i]), len(soundex_l[j]))):
-        #print(inp_data[i],'&',inp_data[j],soundex_l[i][el],soundex_l[j][el],'thund3r')
-        ## print('ld',jlfsh.damerau_levenshtein_distance(soundex_l[i][el], soundex_l[j][el]))
-        ## print('jaro',jlfsh.jaro_distance(soundex_l[i][el], soundex_l[j][el]))
-        #print('hamming',jlfsh.hamming_distance(soundex_l[i][el], soundex_l[j][el]))
-        ## print()
-# what I'm seeing is that the soundex tokens aren't good indicators of similarity. - Dynamic & Engagement are paired 2/4 [D552 E522]
-# therefore i'll move on, look at the notebook and see what's good.
-# maybe i'll try to amalgamate these to programs.
-# where it was useful was below..in accounting for special characters.
+# print(close_ld)
 
-#a counter for r_iter, so that the iterations of the duplicate remover function dont get out of range.
+
+###remove duplicates (geotargetting, google ad thing)
 r_iter=0
 for i,j in zip(range(0,len(soundex_l)-1), range(1,len(soundex_l))):
     if soundex_l[i]==soundex_l[j]:
         r_iter+=1
-
-###remove duplicates (geotargetting, google ad thing)
 for i,j in zip(range(0,len(soundex_l)-r_iter-1), range(1,len(soundex_l)-r_iter)):
     if soundex_l[i]==soundex_l[j]:
         r_iter+=1
-        print(soundex_l[i],'&&&',soundex_l[j],i_l[i],'+++',i_l[j])
+        # print(soundex_l[i],'&&&',soundex_l[j],i_l[i],'+++',i_l[j])
         i_l.remove(i_l[i])
         soundex_l.remove(soundex_l[i])
-        print('999',i_l)
-#--------------
-print('ttt',i_l)
+        # print('item removed',i_l)
+
 #this block is great. it takes a nested list of
     #strings which are within the list separated by commas.
         #it then joins the separate strings together, spaced by a space in this case
         #and appended to a new list.
+# print('markin',i_l[0],i_l[0][0],i_l) #granularity is by word
 i_l_out=[]
 for i in i_l:
-    j= " ".join(i)
-    print(j)
+    j= " ".join(i) #join replaces commas with " " j = i.replace(',',' ') DOES not work, i believe replace only works within a string, rather than between strings. #str.replace == str.split().join()
+    # print(j)
     i_l_out.append(j)
-# print('21',i_l_out)
+# print('markout',i_l_out[0],i_l_out) #granularity is by letter
 # print(len(i_l_out),len(inp_data),len(i_l),len(soundex_l),type(inp_data),type(i_l))
 
 dicto=dict(zip(i_l_out,soundex_l))
 print(dicto)
 
 df=pd.DataFrame.from_dict(dicto,orient='index')
+# print(len(df),'\n\n\n',df)
+# print(df.loc[:,0])
+
+# print(df[0],df[1],df[2],df[3],df[4]) # turns index into a column of it's own, new index is a numbered index (0->42,len=43)
+
+#iamlegend
+# print(df[0:5]) #first 5 rows all columns
+# print(df['index'][1]) #gives first column...must include ' '
+# print(df[0]) #gives first column...must include ' '
+df.reset_index(inplace=True)
+df.reset_index(inplace=True)
+
 print(df)
+dupe=[]
+j='A330'
+print(df[0][4])
+for i in range(len(df[0])-1):
+    j=df[0][i+1]
+    print('iii',i)
+    if jlfsh.damerau_levenshtein_distance(df[0][i], j) <3:
+        print(df[0][i],j)
+        dupe.append(df['index'][i]+' '+df[0][i])#+df[0][j])
+        j=df[0][i-1]
+        
+print('dupe',dupe)
+#
+
+#itertools
+# dupes=[]
+# for i, token,combos in itertools.combinations(df[0],2):
+#     print('1000',token,combos)
+#     if token is not None and combos is not None:
+#         d_ld= jlfsh.damerau_levenshtein_distance(token, combos)
+#         # print(df[0])
+#         if d_ld>1:
+#             continue
+#         else:
+#             # print(token,'<',combos,'>',d_ld) #df['index'][token]
+#             # dupes.append(token+' '+combos) #df['index']
+#             for token,combos in itertools.combinations(df[1],2):
+#                 if token is not None and combos is not None:
+#                     d_ld= jlfsh.damerau_levenshtein_distance(token, combos)
+#                     if d_ld>1:
+#                         continue
+#                     else:
+#                         print(token,combos,d_ld)
+#                         dupes.append(token+' '+combos) #df['index']
+# print('MARKOUT',len(dupes),dupes)
+
+
+
+# for i in range(len(df)):
+#     print(df[0])
 df.to_csv(r"C:\Users\prozehnal\Desktop\csv_house\csv\outputSoundex.csv")
